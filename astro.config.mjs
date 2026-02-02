bb9f9235a9e03dfd3332dd229842f4dcd24ce445
romanogus <96738983+romanogus@users.noreply.github.com>
2026-02-02 15:47:03 -0300

    feat: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), sanity({
+    projectId: 'ljwrfio9',
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: '2026-01-28',
+    studioBasePath: '/studio'
+  }), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
