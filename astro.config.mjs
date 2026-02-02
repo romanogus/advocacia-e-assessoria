95a42af3ab6ea2fda721b0d78dce3315bb264533
romanogus <96738983+romanogus@users.noreply.github.com>
2026-02-02 16:19:08 -0300

    chore: added adapter for vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [svelte(), sanity({
     projectId: 'ljwrfio9',
     dataset: 'production',
