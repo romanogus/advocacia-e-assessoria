35236c7479ef671cb161044a9aa57e92be3a6c8b
romanogus <96738983+romanogus@users.noreply.github.com>
2026-06-11 00:34:02 +0100

    fix: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,18 +8,28 @@ import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [svelte(), sanity({
-    projectId: 'ljwrfio9',
-    dataset: 'production',
-    useCdn: true,
-    apiVersion: '2026-01-28',
-    studioBasePath: '/studio'
-  }), react()],
+  integrations: [
+    svelte(),
+    sanity({
+      projectId: 'ljwrfio9',
+      dataset: 'production',
+      useCdn: true,
+      apiVersion: '2026-01-28',
+      studioBasePath: '/studio'
+    }),
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
